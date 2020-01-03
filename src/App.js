78b4f939f68c56096444ea10f535a77c8557d0ec
morvaridmirkhani <50139669+morvaridmirkhani@users.noreply.github.com>
2020-01-03 11:31:39 +0100

    Create applicant-detail-page (#22)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import IconContainer from "./components/IconContainer";
 import { Switch, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Error404 from "./components/Error404";
+import ApplicantDetailPage from "./components/ApplicantDetailPage";
 
 function App() {
   // The fake data instead of the data which should get from DB.
@@ -13,6 +14,7 @@ function App() {
     <div>
       <Switch>
         <Route path="/" component={Welcome} exact />
+        <Route path="/applicant/:id" component={ApplicantDetailPage} />
         <Route path="*" component={Error404} />
       </Switch>
     </div>

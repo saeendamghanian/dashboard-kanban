f6ef6106a858895d86e8db84af7ae203768530e4
Emanuele De Cupis <ema.decu@gmail.com>
2020-01-07 15:35:51 +0100

    Students api services (#29)
    * fixed broken tests
    
    * added mock student services

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { Switch, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Error404 from "./components/Error404";
 import ApplicantDetailPageContainer from "./components/ApplicantDetailPageContainer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/" component={Welcome} exact />
-        <Route path="/applicant/:id" component={ApplicantDetailPageContainer} />
-        <Route path="*" component={Error404} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Welcome} exact />
+          <Route path="/applicant/:id" component={ApplicantDetailPageContainer} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

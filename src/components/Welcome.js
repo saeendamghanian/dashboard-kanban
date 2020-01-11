4604dbed784bfb5d2abbe8a90e57445d1300081d
Perasperad <you@example.com>
2020-01-11 12:28:50 +0100

    edit the welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,6 @@ import React from "react";
 const Welcome = () => {
   return (
     <div>
-      {" "}
       <h1 style={{ textAlign: "center" }}>Welcome to the KANBAN project</h1>
     </div>
   );

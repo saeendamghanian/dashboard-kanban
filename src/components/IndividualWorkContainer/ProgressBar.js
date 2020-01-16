7a1b2dba102e970a4a26cdda6896d4589aa5729c
Saeen-Damghanian <saeen.damghanian@gmail.com>
2020-01-16 19:03:43 +0100

    Create progress bar

--- src/components/IndividualWorkContainer/ProgressBar.js
+++ src/components/IndividualWorkContainer/ProgressBar.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Stepper, Step, StepLabel } from "@material-ui/core";
 
 const ProgressBar = () => {
-  return <div></div>;
+  const steps = [
+    "step1",
+    "step2",
+    "step3",
+    "step4",
+    "step5",
+    "step6",
+    "step7",
+    "step8"
+  ];
+  const currentStep = 4;
+
+  return (
+    <div>
+      <Stepper activeStep={currentStep} alternativeLabel>
+        {steps.map(label => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
+  );
 };
 
 export default ProgressBar;
--- src/containers/ApplicantDetailPageContainer.js
+++ src/containers/ApplicantDetailPageContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ApplicantDetailPageTitle from "../components/ApplicantDetailPageTitle";
-
+import IndividualWorkContainer from "../components/IndividualWorkContainer";
 class ApplicantDetailPageContainer extends React.Component {
   render() {
     const { params } = this.props.match;
 
-    return <ApplicantDetailPageTitle params={params} />;
+    return (
+      <div>
+        <ApplicantDetailPageTitle params={params} />
+        <IndividualWorkContainer />
+      </div>
+    );
   }
 }
 

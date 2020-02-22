5e8abfc53234163b2470a19fc1b330a829a5e924
Saeen-Damghanian <saeen.damghanian@gmail.com>
2020-02-22 14:41:53 +0100

    Add description to welcome page for GitHub

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>Welcome to the KANBAN project</h1>
+      <h3 style={{ textAlign: "center" }}>
+        Go to <Link to="/dashboard-kanban/kanban">Kanban View</Link> page
+      </h3>
     </div>
   );
 };

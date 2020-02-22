06da9c8f3e68e374a09383cb530f637b0cc415d6
Saeen-Damghanian <saeen.damghanian@gmail.com>
2020-02-22 14:43:02 +0100

    Updates

--- precache-manifest.0abf894f38484fb1e633096438450c35.js
+++ precache-manifest.0abf894f38484fb1e633096438450c35.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45c3dff76e96fa0063bda4d81430b785",
+    "revision": "154e2d10224415d8d895f36826ac8a3e",
     "url": "/dashboard-kanban/index.html"
   },
   {
-    "revision": "6bf7c8d19682b783976e",
+    "revision": "8ff5d310bf54aaee877a",
     "url": "/dashboard-kanban/static/css/2.f90f0d6c.chunk.css"
   },
   {
-    "revision": "8b33838249fe1b42a28b",
+    "revision": "696789a336ad92899dd3",
     "url": "/dashboard-kanban/static/css/main.7ea8f3d1.chunk.css"
   },
   {
-    "revision": "6bf7c8d19682b783976e",
-    "url": "/dashboard-kanban/static/js/2.ac8e129c.chunk.js"
+    "revision": "8ff5d310bf54aaee877a",
+    "url": "/dashboard-kanban/static/js/2.c04e766a.chunk.js"
   },
   {
     "revision": "77d9a5a8d0635e146e324553e360082f",
-    "url": "/dashboard-kanban/static/js/2.ac8e129c.chunk.js.LICENSE"
+    "url": "/dashboard-kanban/static/js/2.c04e766a.chunk.js.LICENSE"
   },
   {
-    "revision": "8b33838249fe1b42a28b",
-    "url": "/dashboard-kanban/static/js/main.f22867af.chunk.js"
+    "revision": "696789a336ad92899dd3",
+    "url": "/dashboard-kanban/static/js/main.55139015.chunk.js"
   },
   {
     "revision": "9cdd64949ba850144dd0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard-kanban/precache-manifest.05914c06a5045532239eeea351ecedfd.js"
+  "/dashboard-kanban/precache-manifest.0abf894f38484fb1e633096438450c35.js"
 );
 
 self.addEventListener('message', (event) => {

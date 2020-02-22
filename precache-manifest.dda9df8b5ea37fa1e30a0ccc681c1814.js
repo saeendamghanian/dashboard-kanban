d395237c7d0f12cbdeb4a680a9c49a0ae2e64ccc
Saeen-Damghanian <saeen.damghanian@gmail.com>
2020-02-22 14:05:41 +0100

    Updates

---
+++ precache-manifest.dda9df8b5ea37fa1e30a0ccc681c1814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c779b8e299c460609681a5b6cc0a409d",
+    "url": "/dashboard-kanban/index.html"
+  },
+  {
+    "revision": "6bf7c8d19682b783976e",
+    "url": "/dashboard-kanban/static/css/2.f90f0d6c.chunk.css"
+  },
+  {
+    "revision": "3f22cccee42c91711fcb",
+    "url": "/dashboard-kanban/static/css/main.7ea8f3d1.chunk.css"
+  },
+  {
+    "revision": "6bf7c8d19682b783976e",
+    "url": "/dashboard-kanban/static/js/2.ac8e129c.chunk.js"
+  },
+  {
+    "revision": "77d9a5a8d0635e146e324553e360082f",
+    "url": "/dashboard-kanban/static/js/2.ac8e129c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f22cccee42c91711fcb",
+    "url": "/dashboard-kanban/static/js/main.58303197.chunk.js"
+  },
+  {
+    "revision": "9cdd64949ba850144dd0",
+    "url": "/dashboard-kanban/static/js/runtime-main.56851537.js"
+  }
+]);
